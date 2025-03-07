5574673f059ddd1ae34cfe3c410b2287e24bb17f
Abdullah Khan <Abdullahk11051@gmail.com>
2025-03-07 16:47:10 -0500

    updated ports

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ const serveHadiths = async (req, res) => {
 
 app.get("/api/hadiths", serveHadiths);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
